7fa0e9fd7a47bf88a491573778c69d101e1d78ab
rhoverholt <rhoverholt@gmail.com>
2021-11-14 02:05:42 -0500

    Implemented initial question logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 // Define global constants and variables
 const startTime = 30;
 
+const question1 = {
+    questionText: "What is 5+3",
+    answerChoices: ["-8", "8", "2", "-2"],
+    answerIndex: 1
+};
+
+const question2 = {
+    questionText: "How much do you like programming?",
+    answerChoices: ["More than sliced bread", "About the same as sliced bread", "It's ok", "About the same as going to the dentist", "Worse than going to the dentist"],
+    answerIndex: 0
+};
+
+const questions = [question1, question2];
+
 let navHighScoreArea = document.getElementById('nav-high-score');
 let timerArea = document.getElementById('timer-container');
 let mainArea = document.getElementById('main-container');
@@ -12,20 +26,20 @@ init();
 // Setup Event Handlers via Delegation
 bodyArea.addEventListener("click", processClick);
 
-
 // Define Event Handler Functions
 function processClick(event) {
     var element = event.target;
 
     switch (element.id) {
         case "start-quiz-btn":
-            console.log("Start button pressed!");
+            startQuiz();
             break;
         case "nav-btn":
             console.log("Nav button pressed!");
             break;
         default:
             console.log("What?! " + element.id + " was clicked?");
+            break;
     }
 }
 
@@ -70,6 +84,31 @@ function displayTimer(time) {
             timerArea.innerHTML = `Time: ${time}`;
         }
     }
+}
+
+function startQuiz() {
+
+    // Pose a random question
+    poseQuestion(Math.floor(Math.random() * questions.length));
+//    startTimer(startTime);
+
+    function poseQuestion(index) {
+        // The questions have been setup as constant objects at the top.
+        // they contain 3 properties: questionText, answerChoices (array), and answer Index (the index to the correct answer)
+
+        var thisQuestion = questions[index];
+        var thisAnswer = 0;
+
+        questionHTML = "<p class='question-header'>" + thisQuestion.questionText + "</p>";
+
+        while (thisAnswer < thisQuestion.answerChoices.length) {
+            questionHTML += `<button id="answer-btn-${thisAnswer}" class="answer-btn" data-this="${index}" data-asked="[${index}"]>${thisQuestion.answerChoices[thisAnswer]}</button>`;
+            thisAnswer++;
+        }
+
+        console.log(questionHTML);
+        mainArea.innerHTML = questionHTML;
+     }
 }
 
 function errorMsg(msg) {
